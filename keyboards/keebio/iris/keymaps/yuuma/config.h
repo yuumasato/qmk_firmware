267b756329d3a60e2fd5808f0cc6c3392739c420
Watson Sato <yuuma.sato@gmail.com>
2021-02-28 14:32:42 +0100

    Fix timing layer taping timing issues
    The shorter tapping term fixes isseus when quickly releasing the Lower
    layer key.

--- keyboards/keebio/iris/keymaps/yuuma/config.h
+++ keyboards/keebio/iris/keymaps/yuuma/config.h
@@ -1,2 +1,4 @@
 #define ONESHOT_TAP_TOGGLE 3  /* Tapping this number of times holds the key until tapped once again. */
 #define ONESHOT_TIMEOUT 3000  /* Time (in ms) before the one shot key is released */
+
+#define TAPPING_TERM 100
